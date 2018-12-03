8dfc7921f51a6f1c8f02518b92b1dac17f05bee8
Alejandro Caires <4990971+ajdafonte@users.noreply.github.com>
2018-12-03 20:04:45 +0100

    Add powershell script

---
+++ imgbb_downloader.ps1
@@ -0,0 +1,16 @@
+﻿#define variables
+#<embed_codes_from_imgbb> = @("https://i.ibb.co/asdad/IMG-2018052.jpg", ...)
+$imgbb_embedcodes = @(<embed_codes_from_imgbb>) #this should be replace before executing the script
+$output_dir = "$PSScriptRoot\"
+
+Write-Host '>> Start download of images from imgbb'
+
+#download images to outputdir with original filename
+foreach ($element in $imgbb_embedcodes) {
+    $filename = Split-Path -Path $($element) -Leaf;
+    Write-Host "Downloading file: $($filename)";
+    $output_file = $output_dir + $filename;
+    Invoke-WebRequest $element -OutFile $output_file
+}
+
+Write-Host '>> End download of images from imgbb'
